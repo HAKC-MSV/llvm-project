259c5fe17152aa7f18fb2ca73088ee9953b15e22
Allan Nesathurai <Allan.Nesathurai@ll.mit.edu>
2025-01-24 22:33:11 +0000

    updated tests to work in test instead of compiler-rt; tests can execute but hang on python server

--- llvm/test/Transforms/Compartmentalization/hakc-base-test.c
+++ llvm/test/Transforms/Compartmentalization/hakc-base-test.c
@@ -1,6 +1,6 @@
 // Make sure llvm ir contains the line '%1 = alloca i32, align 4'
 
-// RUN: %clangxx_hakc %s -S -emit-llvm -o %t.ll
+// RUN: clang %s -S -emit-llvm -o %t.ll
 // RUN: cat %t.ll | FileCheck %s || exit 1
 
 int x, y, z;
