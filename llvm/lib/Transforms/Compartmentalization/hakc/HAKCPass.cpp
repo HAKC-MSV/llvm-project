d12e666f980451850b3306b8c5d032a936be8e6c
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-03-06 14:11:57 -0500

    improving output

--- llvm/lib/Transforms/Compartmentalization/hakc/HAKCPass.cpp
+++ llvm/lib/Transforms/Compartmentalization/hakc/HAKCPass.cpp
@@ -68,7 +68,7 @@ namespace llvm {
         std::error_code err;
         err = sys::fs::create_directories(sys::path::parent_path(Path));
         if (err) {
-            errs() << "Failed to create " << sys::path::parent_path(Path) << "\n";
+            CommonHAKCAnalysis::getWriter(true) << "Failed to create " << sys::path::parent_path(Path) << "\n";
             throw std::exception();
         }
         raw_fd_ostream out(Path, err);
@@ -76,7 +76,7 @@ namespace llvm {
             HAKCAnalysis.GetSystemInfo().GetTypeIdentifier().OutputYAML(out);
             out.close();
         } else {
-            errs() << "Failed to open " << Path << "\n";
+            CommonHAKCAnalysis::getWriter(true) << "Failed to open " << Path << "\n";
             throw std::exception();
         }
 
@@ -85,7 +85,7 @@ namespace llvm {
 
     bool RunHAKCAnalysis(Module &M) {
         if (HAKC_CONFIG_PATH.empty()) {
-            errs() << "HAKC_CONFIG_PATH parameter '-mllvm -HAKC_CONFIG=somepath' not specifiecd\n";
+            CommonHAKCAnalysis::getWriter(true) << "no hakc-config pass specified\n";
             throw std::exception();
         }
         CommonHAKCAnalysis HAKCAnalysis(M, HAKC_CONFIG_PATH);
@@ -96,7 +96,7 @@ namespace llvm {
             case RunCompartmentalization:
                 return runCompartmentalization(HAKCAnalysis);
             default:
-                errs() << "Failed to get valid PassMode (this should never be called)\n";
+                CommonHAKCAnalysis::getWriter(true) << "Invalid HAKC pass mode\n";
                 throw std::exception();
         }
     }
