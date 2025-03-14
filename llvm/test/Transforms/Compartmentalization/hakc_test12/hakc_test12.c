2d23d435c5c42cd3070592765f3a922253a4a479
AL32163 <Allan.Nesathurai@ll.mit.edu>
2025-03-14 22:59:53 +0000

    Updating tests; Tests 4, 5, 7, 10 fail the rest pass.

---
+++ llvm/test/Transforms/Compartmentalization/hakc_test12/hakc_test12.c
@@ -0,0 +1,29 @@
+// RUN: %HAKC_EMIT_LLVM
+// RUN: %HAKC_PROCESS_YAML_COMP_CONFIG
+// RUN: %HAKC_PROCESS_YAML_COMP_DAG_CONFIG
+// RUN: %HAKC_PROCESS_YAML_POLICY_CONFIG
+// RUN: %HAKC_RUN_DAG_PASS
+// RUN: %HAKC_PYTHON_VENV
+// RUN: %HAKC_RUN_PYTHON_DAG
+// RUN: %HAKC_START_POLICY_SERVER & sleep 1 &&\
+// RUN: %HAKC_RUN_COMP_PASS
+// RUN: %HAKC_EVALUATE
+
+// Test of custom transfer function (e.g., sk_buff should call hakc_transfer_skb)
+
+struct sk_buff {
+  unsigned int len;
+
+};
+
+int sk_buff_caller(struct sk_buff* buff) {
+  buff->len = 1;
+  return 0;
+}
+
+// CHECK-LABEL: @HAKC_XFER_sk_buff_caller
+// CHECK: call i32 @get_hakc_address_color(ptr %0)
+// CHECK-NOT: @hakc_transfer_to_clique
+// CHECK: call ptr @hakc_transfer_skb(ptr %0, i64 1, i64 13)
+// CHECK: call i32 @HAKC_ORIG_sk_buff_caller(ptr %3)
+// CHECK: call void @hakc_color_address(ptr %0, i32 %2, i64 32)
