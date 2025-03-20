659b1067aab587dc10e041a584f693f5880c65a1
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-03-20 09:20:20 -0400

    adding support for local configs in source tree

--- llvm/test/Transforms/Compartmentalization/hakc/tests/hakc_test12/hakc_test12.c
+++ llvm/test/Transforms/Compartmentalization/hakc/tests/hakc_test12/hakc_test12.c
@@ -19,5 +19,5 @@ int sk_buff_caller(struct sk_buff* buff) {
 // CHECK: call i32 @get_hakc_address_color(ptr %0)
 // CHECK-NOT: @hakc_transfer_to_clique
 // CHECK: call ptr @hakc_transfer_skb(ptr %0, i64 1, i64 13)
-// CHECK: call i32 @HAKC_ORIG_sk_buff_caller(ptr %3)
-// CHECK: call void @hakc_color_address(ptr %0, i32 %2, i64 32)
+// CHECK: call i32 @HAKC_ORIG_sk_buff_caller(ptr %2)
+// CHECK: call void @hakc_color_address(ptr %0, i32 %1, i64 32)
--- llvm/test/Transforms/Compartmentalization/hakc/tests/hakc_test8/hakc_test8.c
+++ llvm/test/Transforms/Compartmentalization/hakc/tests/hakc_test8/hakc_test8.c
@@ -19,23 +19,27 @@ struct data_struct2 {
 
 int foo(struct data_struct2 *a, int* v1, int* v2) {
     if (a) {
-//        *v1++;
-//        *v2++;
+// CHECK: call ptr @check_hakc_data_access(ptr %1, i64 4, i64 270336)
+        *v1++;
+// CHECK: call ptr @check_hakc_data_access(ptr %4, i64 4, i64 270336)
+        *v2++;
         struct data_struct b;
         b.a = 0;
-        return a->f(&b); // this line is what causes the pass to fail 
+// CHECK: all ptr @check_hakc_data_access(ptr %7, i64 4, i64 270336)
+// CHECK: call ptr @check_hakc_code_access(ptr %10, i64 4, i64 270336, ptr @entry_tokens_4, i64 1)
+        return a->f(&b);
     }
     return 0;
 }
 
 // CHECK-LABEL: i32 @HAKC_XFER_foo(ptr noundef %0, ptr noundef %1, ptr noundef %2)
 // CHECK: call i32 @get_hakc_address_color(ptr %0)
-// CHECK: call ptr @hakc_transfer_to_clique(ptr %0, i64 64, i64 1, i64 13, i1 false)
+// CHECK: call ptr @hakc_transfer_to_clique(ptr %0, i64 64, i64 4, i64 13, i1 false)
 // CHECK: call i32 @get_hakc_address_color(ptr %1)
-// CHECK: call ptr @hakc_transfer_to_clique(ptr %1, i64 32, i64 1, i64 13, i1 false)
+// CHECK: call ptr @hakc_transfer_to_clique(ptr %1, i64 32, i64 4, i64 13, i1 false)
 // CHECK: call i32 @get_hakc_address_color(ptr %2)
-// CHECK: call ptr @hakc_transfer_to_clique(ptr %2, i64 32, i64 1, i64 13, i1 false)
-// CHECK: call i32 @HAKC_ORIG_foo(ptr %5, ptr %7, ptr %9)
-// CHECK: call void @hakc_color_address(ptr %2, i32 %8, i64 32)
-// CHECK: call void @hakc_color_address(ptr %1, i32 %6, i64 32)
-// CHECK: call void @hakc_color_address(ptr %0, i32 %4, i64 64)
+// CHECK: call ptr @hakc_transfer_to_clique(ptr %2, i64 32, i64 4, i64 13, i1 false)
+// CHECK: call i32 @HAKC_ORIG_foo(ptr %4, ptr %6, ptr %8)
+// CHECK: call void @hakc_color_address(ptr %2, i32 %7, i64 32)
+// CHECK: call void @hakc_color_address(ptr %1, i32 %5, i64 32)
+// CHECK: call void @hakc_color_address(ptr %0, i32 %3, i64 64)
