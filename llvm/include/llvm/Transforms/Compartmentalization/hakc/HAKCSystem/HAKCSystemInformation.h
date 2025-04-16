b559ba8651091852c48174ee1be40892fe161ce8
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-04-16 12:43:22 -0400

    Revert "Updating tictac yaml"
    This reverts commit 27c3c4f4

--- llvm/include/llvm/Transforms/Compartmentalization/hakc/HAKCSystem/HAKCSystemInformation.h
+++ llvm/include/llvm/Transforms/Compartmentalization/hakc/HAKCSystem/HAKCSystemInformation.h
@@ -31,7 +31,7 @@ typedef SmallVector<llvm::hakc::pre_transfer_action_def_t>
     HAKCPreTransferActionList;
 typedef SmallVector<llvm::hakc::post_target_action_def_t>
     HAKCPostTargetActionList;
-typedef std::vector<std::shared_ptr<TICTACEpoch>> epoch_vec_t;
+// typedef std::vector<std::shared_ptr<TICTACEpoch>> epoch_vec_t;
 // typedef std::map<std::shared_ptr<HAKCSymbol>, epoch_vec_t> symbol_epoch_map_t;
 
 namespace llvm::hakc {
