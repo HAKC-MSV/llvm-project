aa6432666310000f177e427f5bf1533ef162221d
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-09-26 12:14:56 -0400

    adding comments

--- llvm/include/llvm/Transforms/Compartmentalization/hakc/HAKCPass.h
+++ llvm/include/llvm/Transforms/Compartmentalization/hakc/HAKCPass.h
@@ -9,18 +9,15 @@
 /// compartmentalization pass.
 ///
 //===----------------------------------------------------------------------===//
-//
-// Created by derrick on 3/16/21.
-//
 #ifndef PMC_HAKCPASS_H
 #define PMC_HAKCPASS_H
 
 #include "llvm/IR/PassManager.h"
 
 namespace llvm {
-class HAKCPass : public PassInfoMixin<HAKCPass> {
-public:
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
-};
+    class HAKCPass : public PassInfoMixin<HAKCPass> {
+    public:
+        PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
+    };
 } // namespace llvm
 #endif // PMC_HAKCPASS_H
