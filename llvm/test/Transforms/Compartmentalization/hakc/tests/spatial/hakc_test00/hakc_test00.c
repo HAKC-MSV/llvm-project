950169840ae7201272932c091b0a9bbc4e338f98
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-10-06 16:42:45 -0400

    fixing some bugs in tests

--- llvm/test/Transforms/Compartmentalization/hakc/tests/spatial/hakc_test00/hakc_test00.c
+++ llvm/test/Transforms/Compartmentalization/hakc/tests/spatial/hakc_test00/hakc_test00.c
@@ -17,7 +17,7 @@ int bar(struct data_struct *);
 int foo(struct data_struct *a) {
   if (a) {
 // CHECK-LABEL: if.then
-// CHECK: call ptr @check_hakc_data_access(ptr %1, i64 2, i64 139264)
+// CHECK: call ptr @check_hakc_data_access(ptr %1, i64 1, i64 139264)
     (a->a)++;
 // CHECK: call i32 @bar(ptr noundef %5)
     return bar(a);
@@ -27,6 +27,6 @@ int foo(struct data_struct *a) {
 
 // CHECK-LABEL: i32 @HAKC_XFER_foo(ptr noundef %0)
 // CHECK: call i32 @get_hakc_address_color(ptr %0)
-// CHECK: call ptr @hakc_transfer_to_clique(ptr %0, i64 4, i64 2, i64 13, i1 false)
+// CHECK: call ptr @hakc_transfer_to_clique(ptr %0, i64 4, i64 1, i64 13, i1 false)
 // CHECK: call i32 @HAKC_ORIG_foo(ptr %2)
 // CHECK: call void @hakc_color_address(ptr %0, i32 %1, i64 4)
