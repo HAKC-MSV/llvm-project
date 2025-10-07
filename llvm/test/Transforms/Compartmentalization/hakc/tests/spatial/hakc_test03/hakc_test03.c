3f6c934eadc85fd018e7c9a4e6d168e29d1cffc1
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-10-07 12:03:44 -0400

    test 3 works

--- llvm/test/Transforms/Compartmentalization/hakc/tests/spatial/hakc_test03/hakc_test03.c
+++ llvm/test/Transforms/Compartmentalization/hakc/tests/spatial/hakc_test03/hakc_test03.c
@@ -22,6 +22,6 @@ int foo(struct data_struct *a) {
 
 // CHECK-LABEL: i32 @HAKC_XFER_foo(ptr noundef %0) #0
 // CHECK: call i32 @get_hakc_address_color(ptr %0)
-// CHECK: call ptr @hakc_transfer_to_clique(ptr %0, i64 4, i64 2, i64 13, i1 false)
+// CHECK: call ptr @hakc_transfer_to_clique(ptr %0, i64 4, i64 1, i64 13, i1 false)
 // CHECK: call i32 @HAKC_ORIG_foo(ptr %2)
 // CHECK: call void @hakc_color_address(ptr %0, i32 %1, i64 4)
