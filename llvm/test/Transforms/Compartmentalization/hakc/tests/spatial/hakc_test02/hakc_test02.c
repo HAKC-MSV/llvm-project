3e1e5f470d572356ce74fb9b4cafbe62bbdbcef6
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-10-07 11:59:23 -0400

    fixing test 2

--- llvm/test/Transforms/Compartmentalization/hakc/tests/spatial/hakc_test02/hakc_test02.c
+++ llvm/test/Transforms/Compartmentalization/hakc/tests/spatial/hakc_test02/hakc_test02.c
@@ -16,7 +16,7 @@ struct data {
 };
 
 void init_data(struct data *data) {
-// CHECK: call ptr @check_hakc_data_access(ptr %0, i64 1, i64 73728)
+// CHECK: call ptr @check_hakc_data_access(ptr %0, i64 1, i64 65536)
     data->data = 0;
 // CHECK: store ptr %list, ptr %7
     data->list.next = &data->list;
